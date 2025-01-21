fa077020b1bcbc64227bae09e89d7cff6cbe5059
omargalal20 <oelhanafy12@gmail.com>
2025-01-20 20:50:38 -0800

    refactor: renamed books router file for readability

--- Books/main.py
+++ Books/main.py
@@ -1,11 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
 
-from routers.v1 import books_router
+from routers.v1 import books
 
 app = FastAPI()
 
-app.include_router(books_router.router, prefix="/api/v1", tags=["Books"])
+app.include_router(books.router, prefix="/api/v1", tags=["Books"])
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
