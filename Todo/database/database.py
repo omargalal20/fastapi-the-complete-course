85e74f0ca57d711a7df0b61431dd358e04f977ad
omargalal20 <oelhanafy12@gmail.com>
2025-01-20 20:47:59 -0800

    feat: added env config, database config, and todo table

---
+++ Todo/database/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from config.environment import get_environment_variables
+
+env = get_environment_variables()
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(
+    env.SQLITE_DATABASE_URL, connect_args=connect_args
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
--- Todo/main.py
+++ Todo/main.py
@@ -1,7 +1,18 @@
 import uvicorn
 from fastapi import FastAPI
 
-app = FastAPI()
+from config.environment import get_environment_variables
+from database import database
+from models import todo
+
+env = get_environment_variables()
+
+todo.Base.metadata.create_all(bind=database.engine)
+
+app = FastAPI(
+    title=env.APP_NAME,
+    version=env.APP_VERSION,
+)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
